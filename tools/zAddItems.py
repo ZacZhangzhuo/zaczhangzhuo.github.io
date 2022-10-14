654a9b6908c27cd8181ef047973b90847ad94d04
ZacZhangzhuo <zac.zhangzhuo@gmail.com>
2022-10-14 21:27:45 +0200

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is ahead of 'origin/main' by 1 commit.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	new file:   tools/zAddItems.py
    	new file:   tools/zHTMLModifier.py
    	modified:   zMarkdownStyles.css

---
+++ tools/zAddItems.py
@@ -0,0 +1,16 @@
+"""
+!Example
+   <a href="2020-03-11 建筑思维与手机选择1/建筑思维与手机选择1.html">
+        <img  class="items" src="2020-03-11 建筑思维与手机选择1/images/0001.jpg" alt="建筑思维与手机选择">
+      </a>
+"""
+
+
+str1 = '<a href="'
+str2 = []
+str3 = '">'
+str4 = '<img  class="items" src="'
+str5 = []
+str6 = '" alt="'
+str7 = []
+str8 = '"></a>'
