445f0f4cd244c5ce180f4030e5773974b0327fba
ZacZhangzhuo <zac.zhangzhuo@gmail.com>
2022-10-17 20:57:30 +0200

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   tools/outTemp.html
    	new file:   tools/temp.html
    	modified:   tools/zHTMLModifier.py
    	modified:   "zDesigner/2020-01-10 Logo\347\224\273\345\245\275\357\274\214\345\260\261\347\256\227\345\274\200\345\274\240\344\272\206/Logo\347\224\273\345\245\275\357\274\214\345\260\261\347\256\227\345\274\200\345\274\240\344\272\206.html"
    	modified:   "zDesigner/2020-10-30 Campus+ \342\200\224\342\200\224\350\257\276\347\250\213\350\256\276\350\256\241/Campus+ \342\200\224\342\200\224\350\257\276\347\250\213\350\256\276\350\256\241.html"
    	modified:   "zDesigner/2022-05-01 Section Clipper \342\200\224\342\200\224 \350\257\276\347\250\213\350\256\276\350\256\241/Section Clipper \342\200\224\342\200\224 \350\257\276\347\250\213\350\256\276\350\256\241.html"
    	modified:   zMarkdownStyles.css

--- tools/zHTMLModifier.py
+++ tools/zHTMLModifier.py
@@ -1,6 +1,51 @@
-# import os
-file_path = r'C:\Zac\19 Github\ZacZhangzhuo.github.io\zDesigner\2020-01-10 Logo画好，就算开张了\Logo画好，就算开张了.html'
+import pyperclip as pc
 
-html=open(file_path,'r',encoding="utf-8")
-htmlTexts=html.read()
-print(htmlTexts)
+file_path = (
+    r"C:\Zac\19 Github\ZacZhangzhuo.github.io\zDesigner\2022-05-01 Section Clipper —— 课程设计\Section Clipper —— 课程设计.html"
+)
+
+html = open(file_path, "r", encoding="utf-8")
+htmlTexts = html.read()
+
+
+htmlTexts = htmlTexts.split("<style>")
+tempTexts = htmlTexts[-1].split("</style>")[-1]
+
+htmlTexts = htmlTexts[0] + '<link rel="stylesheet" href="../../zMarkdownStyles.css" />' + tempTexts
+
+# Replace the comment
+htmlTexts = htmlTexts.replace("提供的爬取软件来源于：i.ijrou.com 免费下载使用", "Designer by ArchZ")
+
+
+#Replace the styles
+splittedTexts = htmlTexts.split('style="')
+htmlTexts = splittedTexts[0]
+for i in splittedTexts[1:]:
+    if ( i.find('text-align: center') == -1):
+        htmlTexts = htmlTexts + i.split('"', 1)[1]
+    else:
+        htmlTexts = htmlTexts + 'style="text-align: center;"' + i.split('"', 1)[1]
+    
+    
+# Replace user comments
+beReplaced = """<div class="comment">
+				<h3 style="margin:26px 0;font-weight:100;padding-bottom:4px;border-bottom:1px solid #ccc;">精选留言</h3>
+				用户设置不下载评论
+			</div>"""
+htmlTexts = htmlTexts.replace(
+    beReplaced, '<p class="endnotes">--- Growing, Growing, Brighter Everyday ! ---</p>'
+)
+
+beReplaced = """<div class="comment">
+				<h3 >精选留言</h3>
+				用户设置不下载评论
+			</div>"""
+htmlTexts = htmlTexts.replace(
+    beReplaced, '<p class="endnotes">--- Growing, Growing, Brighter Everyday ! ---</p>'
+)
+
+html = open(file_path, "w", encoding="utf-8")
+# print (index)
+# print(htmlTexts)
+# pc.copy(htmlTexts)
+html.write(htmlTexts)
