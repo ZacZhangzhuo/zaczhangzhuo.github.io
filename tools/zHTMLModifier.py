2a564861a233b5a2e48eb5751181f9f60a0b4ac2
ZacZhangzhuo <zac.zhangzhuo@gmail.com>
2022-10-17 09:07:26 +0200

    On branch main 	modified:   tools/zHTMLModifier.py

--- tools/zHTMLModifier.py
+++ tools/zHTMLModifier.py
@@ -1,7 +1,6 @@
 # import os
 file_path = r'C:\Zac\19 Github\ZacZhangzhuo.github.io\zDesigner\2020-01-10 Logo画好，就算开张了\Logo画好，就算开张了.html'
 
-
 html=open(file_path,'r',encoding="utf-8")
 htmlTexts=html.read()
 print(htmlTexts)
