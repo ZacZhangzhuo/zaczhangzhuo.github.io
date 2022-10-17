8e15b242b1cdb0bf4de684f056bde5307ed16ba3
ZacZhangzhuo <zac.zhangzhuo@gmail.com>
2022-10-17 22:40:33 +0200

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   tools/zHTMLModifier.py
    	deleted:    tools/zMarkdown.css
    	new file:   zCV.html
    	new file:   zContact.html
    	modified:   "zDesigner/2020-01-10 Logo\347\224\273\345\245\275\357\274\214\345\260\261\347\256\227\345\274\200\345\274\240\344\272\206/Logo\347\224\273\345\245\275\357\274\214\345\260\261\347\256\227\345\274\200\345\274\240\344\272\206.html"
    	modified:   "zDesigner/2020-01-12 PARASITE\342\200\224\342\200\224\350\257\276\347\250\213\350\256\276\350\256\241/PARASITE\342\200\224\342\200\224\350\257\276\347\250\213\350\256\276\350\256\241.html"
    	modified:   "zDesigner/2020-10-30 Campus+ \342\200\224\342\200\224\350\257\276\347\250\213\350\256\276\350\256\241/Campus+ \342\200\224\342\200\224\350\257\276\347\250\213\350\256\276\350\256\241.html"
    	modified:   "zDesigner/2021-03-27 ArchZ \344\270\252\344\272\272\347\275\221\347\253\231\344\270\212\347\272\277/ArchZ \344\270\252\344\272\272\347\275\221\347\253\231\344\270\212\347\272\277.html"
    	modified:   "zDesigner/2021-10-08 Megachain\342\200\224\342\200\224\350\257\276\347\250\213\350\256\276\350\256\241/Megachain\342\200\224\342\200\224\350\257\276\347\250\213\350\256\276\350\256\241.html"
    	modified:   "zDesigner/2022-05-01 Section Clipper \342\200\224\342\200\224 \350\257\276\347\250\213\350\256\276\350\256\241/Section Clipper \342\200\224\342\200\224 \350\257\276\347\250\213\350\256\276\350\256\241.html"
    	modified:   zDesigner/zDesigner.html
    	modified:   zIndividualDeveloper/zIndividualDeveloper.html
    	modified:   "zLearner/2020-02-17 Grasshopper-C \346\225\264\347\220\206\342\200\224\342\200\224Script/Grasshopper-C \346\225\264\347\220\206\342\200\224\342\200\224Script.html"
    	modified:   "zLearner/2020-03-02 Rhino\345\255\246\344\271\240\342\200\224\342\200\224\345\267\245\345\205\267\351\233\206/Rhino\345\255\246\344\271\240\342\200\224\342\200\224\345\267\245\345\205\267\351\233\206.html"
    	modified:   "zLearner/2022-09-13 Grasshopper-Python \350\264\252\345\220\203\350\233\207/Grasshopper-Python \350\264\252\345\220\203\350\233\207.html"
    	modified:   zLearner/zLearner.html
    	modified:   zMarkdownStyles.css
    	modified:   zPhotographer/zPhotographer.html
    	new file:   zSkills.html
    	modified:   zStyles.css
    	renamed:    zLearner/2022-09-12 Growing, Growing, Brighter Everyday-/Growing, Growing, Brighter Everyday-.html -> zWriter/2022-09-12 Growing, Growing, Brighter Everyday-/Growing, Growing, Brighter Everyday-.html
    	renamed:    zLearner/2022-09-12 Growing, Growing, Brighter Everyday-/images/0001.jpg -> zWriter/2022-09-12 Growing, Growing, Brighter Everyday-/images/0001.jpg
    	renamed:    zLearner/2022-09-12 Growing, Growing, Brighter Everyday-/images/0002.png -> zWriter/2022-09-12 Growing, Growing, Brighter Everyday-/images/0002.png
    	renamed:    zLearner/2022-09-12 Growing, Growing, Brighter Everyday-/images/0003.png -> zWriter/2022-09-12 Growing, Growing, Brighter Everyday-/images/0003.png
    	renamed:    zLearner/2022-09-12 Growing, Growing, Brighter Everyday-/images/0004.png -> zWriter/2022-09-12 Growing, Growing, Brighter Everyday-/images/0004.png
    	renamed:    zLearner/2022-09-12 Growing, Growing, Brighter Everyday-/images/0005.jpg -> zWriter/2022-09-12 Growing, Growing, Brighter Everyday-/images/0005.jpg
    	modified:   zWriter/zWriter.html

--- tools/zHTMLModifier.py
+++ tools/zHTMLModifier.py
@@ -1,32 +1,63 @@
-import pyperclip as pc
-
-file_path = (
-    r"C:\Zac\19 Github\ZacZhangzhuo.github.io\zDesigner\2022-05-01 Section Clipper —— 课程设计\Section Clipper —— 课程设计.html"
-)
+file_path = r"C:\Zac\19 Github\ZacZhangzhuo.github.io\zDesigner\2020-10-30 Campus+ ——课程设计\Campus+ ——课程设计.html"
 
 html = open(file_path, "r", encoding="utf-8")
 htmlTexts = html.read()
 
+# Add Menu Bar
+menuBar = """<!-- Menu Bar -->
+    <div class="menuBar">
+      <a href="../../zHome.html" class="menuBar"> <p>zHome</p></a>
+      <a href="../../zDesigner/zDesigner.html"class="menuBar"><p>zDesigner</p></a>
+      <a href="../../zLearner/zLearner.html"class="menuBar"><p>zLearner</p></a>
+      <a href="../../zWriter/zWriter.html"class="menuBar"><p>zWriter</p></a>
+      <a href="../../zPhotographer/zPhotographer.html"class="menuBar"><p>zPhotographer</p></a>
+      <a href="../../zIndividualDeveloper/zIndividualDeveloper.html"class="menuBar"><p>zIndividualDeveloper</p></a>
+      <a href="../../zCV.html" class="menuBar"><p>zCV</p></a>
+      <a href="../../zSkills.html" class="menuBar"><p>zSkills</p></a>
+      <a href="../../zContact.html" class="menuBar"><p>zContact</p></a>
+    </div>
+    <!-- End of Menu Bar -->"""
+if file_path.find("zDesigner") != -1:
+    menuBar = menuBar.replace("/../zDesigner/", "/")
+elif file_path.find("zLearner") != -1:
+    menuBar = menuBar.replace("/../zLearner/", "/")
+elif file_path.find("zWriter") != -1:
+    menuBar = menuBar.replace("/../zWriter/", "/")
+elif file_path.find("zPhotographer") != -1:
+    menuBar = menuBar.replace("/../zPhotographer/", "/")
+elif file_path.find("zIndividualDeveloper") != -1:
+    menuBar = menuBar.replace("/../zIndividualDeveloper/", "/")
+elif file_path.find("zCV") != -1:
+    menuBar = menuBar.replace("/../zCV/", "/")
+elif file_path.find("zSkills") != -1:
+    menuBar = menuBar.replace("/../zSkills/", "/")
+elif file_path.find("zContact") != -1:
+    menuBar = menuBar.replace("/../zContact/", "/")
 
-htmlTexts = htmlTexts.split("<style>")
-tempTexts = htmlTexts[-1].split("</style>")[-1]
-
-htmlTexts = htmlTexts[0] + '<link rel="stylesheet" href="../../zMarkdownStyles.css" />' + tempTexts
+htmlTexts = htmlTexts.replace("<body class='appmsg_skin_default'>", "<body>" + menuBar)
 
 # Replace the comment
 htmlTexts = htmlTexts.replace("提供的爬取软件来源于：i.ijrou.com 免费下载使用", "Designer by ArchZ")
 
+# Replace the style sheet link
+htmlTexts = htmlTexts.split("<style>")
+tempTexts = htmlTexts[-1].split("</style>")[-1]
+htmlTexts = htmlTexts[0] + '<link rel="stylesheet" href="../../zMarkdownStyles.css" />' + tempTexts
 
-#Replace the styles
+
+# Replace the styles
 splittedTexts = htmlTexts.split('style="')
 htmlTexts = splittedTexts[0]
 for i in splittedTexts[1:]:
-    if ( i.find('text-align: center') == -1):
+    if i.find("text-align: center") == -1:
         htmlTexts = htmlTexts + i.split('"', 1)[1]
     else:
         htmlTexts = htmlTexts + 'style="text-align: center;"' + i.split('"', 1)[1]
-    
-    
+
+
+# Replace read the article
+htmlTexts = htmlTexts.replace("阅读全文", " ")
+
 # Replace user comments
 beReplaced = """<div class="comment">
 				<h3 style="margin:26px 0;font-weight:100;padding-bottom:4px;border-bottom:1px solid #ccc;">精选留言</h3>
