29488f712ef4490aae1a32095e54c898038df1ea
Zac Zhang <zac.z.zhang@outlook.com>
2022-11-06 15:20:25 +0100

    Add

--- tools/zAddItems.py
+++ tools/zAddItems.py
@@ -11,18 +11,18 @@ import os
 sys.path.append("C:/Zac/GitHub")  # For Zorse
 from Zorse import core_zHTML
 
-target1 = r"C:\Zac\Github\ZacZhangzhuo.github.io\zDesigner"
-target2 = r"C:\Zac\Github\ZacZhangzhuo.github.io\zIndividualDeveloper"
-target3 = r"C:\Zac\Github\ZacZhangzhuo.github.io\zLearner"
-target4 = r"C:\Zac\Github\ZacZhangzhuo.github.io\zWriter"
-target5 = r"C:\Zac\Github\ZacZhangzhuo.github.io\zPhotographer"
+target1 = r"zDesigner"
+target2 = r"zIndividualDeveloper"
+target3 = r"zLearner"
+target4 = r"zWriter"
+target5 = r"zPhotographer"
 IsModifyRecentFile = True  # The html would be modified only when the subdirectory is changed.
 deleteTheFirstOne = (
     True  # This tells if you wanna delete the html like zWriter - You always want to!
 )
 RecentMinutes = 5
-# targets = [target1, target2, target3, target4, target5]
-targets = [target1]
+targets = [target1, target2, target3, target4, target5]
+# targets = [target1]
 
 for target in targets:
     fileNames = []
@@ -36,31 +36,8 @@ for target in targets:
         itemNames.extend(k)
 
     # print(filePaths)
-    """
-    'C:\\Zac\\Github\\ZacZhangzhuo.github.io\\zDesigner', 
-    'C:\\Zac\\Github\\ZacZhangzhuo.github.io\\zDesigner\\2020-01-10 Logo画好，就算开张了', 
-    'C:\\Zac\\Github\\ZacZhangzhuo.github.io\\zDesigner\\2020-01-10 Logo画好，就算开张了\\images', 
-    'C:\\Zac\\Github\\ZacZhangzhuo.github.io\\zDesigner\\2020-01-12 PARASITE——课程设计',
-    ....
-    """
     # print (fileNames)
-    """
-    '2020-01-10 Logo画好，就算开张了',
-    '2020-01-12 PARASITE——课程设计', 
-    '2020-10-30 Campus+ ——课程设计',
-    '2021-03-27 ArchZ 个人网站上线',
-    ....
-    """
     # print (itemNames)
-    """
-    zDesigner.html', 
-    'Logo画好，就算开张了.html',
-    '0001.jpg', 
-    '0002.jpg', 
-    '0003.jpg', 
-    '0004.jpg',
-    ....
-    """
 
     # This tells if you wanna delete the html like zWriter
     if deleteTheFirstOne:
@@ -93,16 +70,19 @@ for target in targets:
     else:
         isChange = True
 
+    # print(filePaths)
+    # print(htmlNames)
+
     if isChange:
         outString = "<!--! ADD NEW ARTICLE HERE -->"
         for i in range(len(htmlNames) - 1, -1, -1):
             str1 = '<a href="'
-            str2 = filePaths[i] + "/" + htmlNames[i]
+            str2 = fileNames[i]+'\\' + htmlNames[i]
             str3 = '">'
             str4 = '<img  class="items" src="'
-            str5 = filePaths[i]
+            str5 = fileNames[i]
             str6 = "/images/" + imageNames[i] + '" alt="'
-            str7 = htmlNames[i].split(".")[0]
+            str7 = fileNames[i].split(".")[0]
             str8 = '"></a>'
             outString = outString + str1 + str2 + str3 + str4 + str5 + str6 + str7 + str8
         outString += "<!-- !End of Items -->"
--- tools/zMenuBarUpdater.py
+++ tools/zMenuBarUpdater.py
@@ -1,31 +1,63 @@
-menuBar = """<!-- Menu Bar -->
-        <div class="menuBar">
-        <a href="../../zHome.html" class="menuBar"> <p>zHome</p></a>
-        <a href="../../zDesigner/zDesigner.html"class="menuBar"><p>zDesigner</p></a>
-        <a href="../../zLearner/zLearner.html"class="menuBar"><p>zLearner</p></a>
-        <a href="../../zWriter/zWriter.html"class="menuBar"><p>zWriter</p></a>
-        <a href="../../zPhotographer/zPhotographer.html"class="menuBar"><p>zPhotographer</p></a>
-        <a href="../../zIndividualDeveloper/zIndividualDeveloper.html"class="menuBar"><p>zIndividualDeveloper</p></a>
-        <a href="../../zCV.html" class="menuBar"><p>zCV</p></a>
-        <a href="../../zSkills.html" class="menuBar"><p>zSkills</p></a>
-        <a href="../../zContact.html" class="menuBar"><p>zContact</p></a>
-        </div>
-        <!-- End of Menu Bar -->"""
-    if file_path.find("zDesigner") != -1:
-        menuBar = menuBar.replace("/../zDesigner/", "/")
-    elif file_path.find("zLearner") != -1:
-        menuBar = menuBar.replace("/../zLearner/", "/")
-    elif file_path.find("zWriter") != -1:
-        menuBar = menuBar.replace("/../zWriter/", "/")
-    elif file_path.find("zPhotographer") != -1:
-        menuBar = menuBar.replace("/../zPhotographer/", "/")
-    elif file_path.find("zIndividualDeveloper") != -1:
-        menuBar = menuBar.replace("/../zIndividualDeveloper/", "/")
-    elif file_path.find("zCV") != -1:
-        menuBar = menuBar.replace("/../zCV/", "/")
-    elif file_path.find("zSkills") != -1:
-        menuBar = menuBar.replace("/../zSkills/", "/")
-    elif file_path.find("zContact") != -1:
-        menuBar = menuBar.replace("/../zContact/", "/")
-    else:
-        menuBar = menuBar.replace("../../z", "/z")
+import os
+MenuBarDir = [
+    "index.html",
+    "zDesigner\zDesigner.html",
+    "zLearner\zLearner.html",
+    "zWriter\zWriter.html",
+    "zPhotographer\zPhotographer.html",
+    "zIndividualDeveloper\zIndividualDeveloper.html",
+    "zCV.html",
+    "zSkills.html",
+    "zContact.html",
+]
+
+# p = "index.html"
+# x= os.path.relpath(MenuBarDir[3], p)[3:]
+# fileNames = []
+# filePaths = []
+# itemNames = []
+# # Get all the files
+# for i, j, k in os.walk("."):
+#     filePaths.append(i)
+#     fileNames.append(j)
+#     itemNames.append(k)
+
+# # Get all the html files and the location
+# htmlPaths = []
+# htmlNames = []
+# for i in range(len(filePaths)):
+#     if ''.join(itemNames).find(".html") != -1:
+#         htmlPaths.append(filePaths[i])
+#         htmlNames.append(itemNames[i])
+
+# menuBar = """<!-- Menu Bar -->
+#         <div class="menuBar">
+#         <a href="../../zHome.html" class="menuBar"> <p>zHome</p></a>
+#         <a href="../../zDesigner/zDesigner.html"class="menuBar"><p>zDesigner</p></a>
+#         <a href="../../zLearner/zLearner.html"class="menuBar"><p>zLearner</p></a>
+#         <a href="../../zWriter/zWriter.html"class="menuBar"><p>zWriter</p></a>
+#         <a href="../../zPhotographer/zPhotographer.html"class="menuBar"><p>zPhotographer</p></a>
+#         <a href="../../zIndividualDeveloper/zIndividualDeveloper.html"class="menuBar"><p>zIndividualDeveloper</p></a>
+#         <a href="../../zCV.html" class="menuBar"><p>zCV</p></a>
+#         <a href="../../zSkills.html" class="menuBar"><p>zSkills</p></a>
+#         <a href="../../zContact.html" class="menuBar"><p>zContact</p></a>
+#         </div>
+#         <!-- End of Menu Bar -->"""
+#     if file_path.find("zDesigner") != -1:
+#         menuBar = menuBar.replace("/../zDesigner/", "/")
+#     elif file_path.find("zLearner") != -1:
+#         menuBar = menuBar.replace("/../zLearner/", "/")
+#     elif file_path.find("zWriter") != -1:
+#         menuBar = menuBar.replace("/../zWriter/", "/")
+#     elif file_path.find("zPhotographer") != -1:
+#         menuBar = menuBar.replace("/../zPhotographer/", "/")
+#     elif file_path.find("zIndividualDeveloper") != -1:
+#         menuBar = menuBar.replace("/../zIndividualDeveloper/", "/")
+#     elif file_path.find("zCV") != -1:
+#         menuBar = menuBar.replace("/../zCV/", "/")
+#     elif file_path.find("zSkills") != -1:
+#         menuBar = menuBar.replace("/../zSkills/", "/")
+#     elif file_path.find("zContact") != -1:
+#         menuBar = menuBar.replace("/../zContact/", "/")
+#     else:
+#         menuBar = menuBar.replace("../../z", "/z")
