c2af18337600f13bea6c4c499457bd3cc241e730
Zac Zhang <zac.z.zhang@outlook.com>
2022-12-10 11:10:58 +0100

    Add

---
+++ zFunctions.js
@@ -0,0 +1,5 @@
+function adjustIframe(){
+        var ifm= document.getElementById("iframe");
+        ifm.height=document.documentElement.clientHeight*0.5;
+        ifm.width=document.documentElement.clientWidth*0.5;
+}
