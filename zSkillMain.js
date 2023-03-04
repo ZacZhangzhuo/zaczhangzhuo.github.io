5f103a02822bca908c35da525cdcd1e77da41007
Zac Zhang <zac.z.zhang@outlook.com>
2023-03-04 17:43:20 +0100

    Add

--- zSkillMain.js
+++ zSkillMain.js
@@ -1,4 +1,4 @@
-import { AmbientLight } from "three";
+// import { AmbientLight } from "three";
 import * as THREE from "/node_modules/three/build/three.module.js";
 import { OrbitControls } from "/node_modules/three/examples/jsm/controls/OrbitControls.js";
 import data from "/zCV.json";
