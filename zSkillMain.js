ee9ffc124f90cc17ebcfd773d05bfdb6747f67bf
Zac Zhang <zac.z.zhang@outlook.com>
2023-04-13 18:36:28 +0200

    Add

--- zSkillMain.js
+++ zSkillMain.js
@@ -1,4 +1,3 @@
-
 import * as THREE from "three";
 import { Vector3 } from "three";
 import { OrbitControls } from "jsm/controls/OrbitControls.js";
@@ -25,16 +24,14 @@ const data = theData;
 //Scene
 const scene = new THREE.Scene();
 
-const textMeshes = [];
-const textCenters = [];
+var fatherTextMeshes = [];
+var sonTextMeshes = [];
 
 const fontLoader = new FontLoader();
 var rise = 0.1;
+
 fontLoader.load("resources/fonts/Source Sans Pro_Italic.json", (font) => {
 	//! Create texts for the fathers
-	// const fatherTextMaterial = new THREE.MeshStandardMaterial({ color: "#ffffff", emissive: "#c7c7c7" });
-	// const fatherTextMaterial = new THREE.MeshStandardMaterial({ color: "#ffffff",transparent : true });
-
 	for (let j = 0; j < data[0].length; j++) {
 		var textMesh = new THREE.Mesh();
 		const textGeometry = new TextGeometry(data[0][j]["Category"], {
@@ -47,16 +44,12 @@ fontLoader.load("resources/fonts/Source Sans Pro_Italic.json", (font) => {
 		textMesh.geometry = textGeometry;
 		textMesh.material = new THREE.MeshStandardMaterial({ color: "#ffffff21" });
 		textMesh.position.set(data[0][j]["Point"][0], data[0][j]["Point"][1], data[0][j]["Point"][2]);
-		textMeshes.push(textMesh);
-		textCenters.push(new Vector3(data[0][j]["Point"][0], data[0][j]["Point"][1], data[0][j]["Point"][2]));
+		fatherTextMeshes.push(textMesh);
 
 		scene.add(textMesh);
 	}
 
 	//! Create texts for the sons
-
-	// const sonTextMaterial = new THREE.MeshStandardMaterial({ color: "#ffffff", transparent: true });
-	const axises = [];
 	for (let j = 0; j < data[1].length; j++) {
 		var textMesh = new THREE.Mesh();
 		const textGeometry = new TextGeometry(
@@ -79,19 +72,15 @@ fontLoader.load("resources/fonts/Source Sans Pro_Italic.json", (font) => {
 		);
 
 		textMesh.geometry = textGeometry;
-
 		textMesh.material = new THREE.MeshStandardMaterial({ color: "#ffffff", transparent: true });
 		textMesh.position.set(data[1][j]["Point"][0], data[1][j]["Point"][1] + rise, data[1][j]["Point"][2]);
-		textMeshes.push(textMesh);
-		textCenters.push(new Vector3(data[1][j]["Point"][0], data[1][j]["Point"][1] + rise, data[1][j]["Point"][2]));
-
+		sonTextMeshes.push(textMesh);
 		scene.add(textMesh);
 	}
 });
 
 //! Draw lines between fathers
-const Lines = [];
-const LineCenters = [];
+var fatherLines = [];
 const fatherLineMaterial = new THREE.LineBasicMaterial({ color: "#ffffff" });
 
 for (let j = 0; j < data[0].length; j++) {
@@ -104,11 +93,13 @@ for (let j = 0; j < data[0].length; j++) {
 	var geometry = new THREE.BufferGeometry().setFromPoints(Points);
 
 	var line = new THREE.Line(geometry, fatherLineMaterial);
-	// Lines.push(line);
+	fatherLines.push(line);
 	scene.add(line);
 }
 
 //! Draw lines between fathers and sons
+var sonLines = [];
+var LineEnds = [];
 for (let j = 0; j < data[0].length; j++) {
 	for (let k = 0; k < data[0][j]["Links"].length; k++) {
 		var v1 = new Vector3(data[0][j]["Point"][0], data[0][j]["Point"][1], data[0][j]["Point"][2]);
@@ -120,29 +111,11 @@ for (let j = 0; j < data[0].length; j++) {
 		var Points = [v1, v2];
 		var geometry = new THREE.BufferGeometry().setFromPoints(Points);
 		var line = new THREE.Line(geometry, new THREE.LineBasicMaterial({ color: "#ffffff", transparent: true }));
-		LineCenters.push(v2);
-		Lines.push(line);
+		LineEnds.push([v1, v2]);
+		sonLines.push(line);
 		scene.add(line);
 	}
 }
-
-/*
-//! Draw beauty lines
-var length = 1;
-var DecLines = [];
-for (let j = 0; j < data[1].length; j++) {
-	var v1 = new Vector3(data[1][j]["Point"][0], data[1][j]["Point"][1]+rise, data[1][j]["Point"][2]);
-	var v2 = new Vector3(data[1][j]["Point"][0], data[1][j]["Point"][1] - length, data[1][j]["Point"][2]);
-	var Points = [v1, v2];
-	var geometry = new THREE.BufferGeometry().setFromPoints(Points);
-	var line = new THREE.Line(geometry, new THREE.LineBasicMaterial({ color: "#7b7b7b", transparent: true }));
-	LineCenters.push(v2);
-	// console.log(LineCenters);
-	DecLines.push(line);
-	Lines.push(line);
-	scene.add(line);
-}
-*/
 
 //! Light
 // const ambientLight = new THREE.AmbientLight("#ffffff", 1);
@@ -178,7 +151,7 @@ controls.minPolarAngle = Math.PI / 2;
 controls.enablePan = false;
 controls.enableZoom = false;
 controls.autoRotate = true;
-controls.autoRotateSpeed = 1;
+controls.autoRotateSpeed = 0.8;
 
 //Resize
 window.addEventListener("resize", () => {
@@ -194,59 +167,52 @@ window.addEventListener("resize", () => {
 
 var maxDistance = 0;
 var minDistance = 10000000;
-for (let i = 0; i < Lines.length; i++) {
-	var dis = camera.position.distanceTo(LineCenters[i]);
+for (let i = 0; i < sonLines.length; i++) {
+	var dis = camera.position.distanceTo(LineEnds[i][0]);
 	if (dis > maxDistance) maxDistance = dis;
 	if (dis < minDistance) minDistance = dis;
 }
-
-//
 
 //! Loop
 var f = 0;
 const loop = () => {
 	f++;
 	controls.update();
-	//	Live update
 	renderer.render(scene, camera);
 	window.requestAnimationFrame(loop);
-
-	for (let i = 0; i < textMeshes.length; i++) {
-		textMeshes[i].lookAt(camera.position);
-	}
-
-	// textMesh.lookAt(camera.position);
 	pointLight.position.set(camera.position.x, camera.position.y, camera.position.z);
 
-	for (let i = 0; i < Lines.length; i++) {
-		var dis = camera.position.distanceTo(LineCenters[i]);
-		Lines[i].material.opacity = 1 - ((dis - minDistance) / (maxDistance - minDistance)) * 2.5;
+	for (let i = 0; i < fatherTextMeshes.length; i++) {
+		fatherTextMeshes[i].lookAt(camera.position);
 	}
 
-	for (let i = 0; i < textMeshes.length; i++) {
-		var dis = camera.position.distanceTo(textCenters[i]);
-		textMeshes[i].material.opacity = 1 - ((dis - minDistance) / (maxDistance - minDistance)) * 2.5;
+	for (let i = 0; i < sonTextMeshes.length; i++) {
+		sonTextMeshes[i].lookAt(camera.position);
 	}
+
+	for (let i = 0; i < sonLines.length; i++) {
+		if (sonLines.length != sonTextMeshes.length) {
+			return;
+		}
+
+		// rotateAroundPoint(sonTextMeshes[i], LineEnds[i][0], new Vector3(0, 1, 0), 0.1);
+		// rotateAroundPoint(sonLines[i], LineEnds[i][0], new Vector3(0, 1, 0), 0.1);
+		
+		
+		var dis = camera.position.distanceTo(LineEnds[i][1]);
+		sonLines[i].material.opacity = 1 - ((dis - minDistance) / (maxDistance - minDistance)) * 2.5;
+		sonTextMeshes[i].material.opacity = 1 - ((dis - minDistance) / (maxDistance - minDistance)) * 2.5;
+	}
+
 };
 loop();
 
-
-
-function rotateAroundPoint(object, point, axis, angle) {
-	// Create a translation vector to move the object so that the point
-	// around which it will rotate becomes the origin
-	const translation = new THREE.Vector3().subVectors(point, object.position);
-
-	// Apply the translation to the object's position
-	object.position.add(translation);
-
-	// Create a rotation matrix to rotate the object around the specified axis
-	const rotation = new THREE.Matrix4().makeRotationAxis(axis.normalize(), angle);
-
-	// Apply the rotation to the object's position and rotation
-	object.position.applyMatrix4(rotation);
-	object.rotation.applyMatrix4(rotation);
-
-	// Reverse the initial translation to move the object back to its original position
-	object.position.sub(translation);
+function rotateAroundPoint(object, point, axis, anglePerFrame) {
+	if (!object) return;
+	const angle = (anglePerFrame * Math.PI) / 180; // Convert degrees to radians
+	const rotationMatrix = new THREE.Matrix4().makeRotationAxis(axis.normalize(), angle);
+	const translationMatrix = new THREE.Matrix4().makeTranslation(point.x, point.y, point.z);
+	const inverseTranslationMatrix = new THREE.Matrix4().makeTranslation(-point.x, -point.y, -point.z);
+	const finalMatrix = translationMatrix.multiply(rotationMatrix).multiply(inverseTranslationMatrix);
+	object.applyMatrix4(finalMatrix);
 }
