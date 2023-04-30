8cab03b96969516c6eb754ef21672434237362d5
Zac Zhang <zac.z.zhang@outlook.com>
2023-04-30 16:15:38 +0200

    v 2.1.0

--- zFunctions.js
+++ zFunctions.js
@@ -8,3 +8,13 @@ function adjustIframe() {
 		ifm.width = document.documentElement.clientWidth * 0.5;
 	}
 }
+function replaceImagesBeforePrint(className) {
+	var images = document.getElementsByClassName(className);
+	for (var i = 0; i < images.length; i++) {
+		var oldSrc = images[i].src;
+		var newSrc = oldSrc.replace(/\.(jpg|jpeg|png|gif)$/i, "_print.$1");
+
+		images[i].src = newSrc;
+	}
+
+}
