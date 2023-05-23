dbf90ec0b0ed02e4e88a8de303edbbad7300e490
Zac Zhang <zac.z.zhang@outlook.com>
2023-05-23 17:07:53 +0200

    Add

--- zFunctions.js
+++ zFunctions.js
@@ -1,11 +1,14 @@
 function adjustIframe() {
-	var ifm = document.getElementById("iframe");
-	if (document.documentElement.clientHeight > document.documentElement.clientWidth) {
-		ifm.height = document.documentElement.clientHeight;
-		ifm.width = document.documentElement.clientWidth;
-	} else {
-		ifm.height = document.documentElement.clientHeight * 0.5;
-		ifm.width = document.documentElement.clientWidth * 0.5;
+	var iframes = document.getElementsByTagName("iframe");
+	for (var i = 0; i < iframes.length; i++) {
+		ifm = iframes[i];
+		if (document.documentElement.clientHeight > document.documentElement.clientWidth) {
+			ifm.height = document.documentElement.clientHeight;
+			ifm.width = document.documentElement.clientWidth;
+		} else {
+			ifm.height = document.documentElement.clientHeight * 0.5;
+			ifm.width = document.documentElement.clientWidth * 0.5;
+		}
 	}
 }
 function replaceImagesBeforePrint(className) {
