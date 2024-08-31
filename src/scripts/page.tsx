ae335600ba49dc43a823a3b84502f781a7231f2d
ZacZhangzhuo <zac.zhangzhuo@gmail.com>
2024-08-31 21:55:41 +0200

    feat: favicon.

--- src/scripts/page.tsx
+++ src/scripts/page.tsx
@@ -22,7 +22,7 @@ export function Page({
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				{meta}
 				<script src="/zFunctions.js" />
-				<link rel="icon" href="/icon.ico" />
+				<link rel="icon" href="/favicon.ico" />
 				<link rel="stylesheet" href="/styles/zStyles.css" />
 				<link rel="stylesheet" href="/styles/zMarkdownStyles.css" />
 				<title>{title}</title>
